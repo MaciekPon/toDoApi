42ced0ec72a87a9126ad9409746b86bc48e7a3d4
MaciejP <m.ponicki1995@gmail.com>
2024-10-02 11:34:26 +0200

    add mongodb

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
+const port = process.env.PORT;
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_URI);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async function (req, res) {
+  try {
+    await client.connect();
+    const db = client.db("todoBase");
+    const collection = db.collection("test");
+
+    // Find the first document in the collection
+    const findResult = await collection.find().toArray();
+    res.send(findResult);
+  } finally {
+    // Close the database connection when finished or an error occurs
+    await client.close();
+  }
 });
 
-app.listen(3001, function () {
-  console.log(`Example app listening on port 3001`);
+app.post("/addTodo", async function (req, res) {
+  try {
+    await client.connect();
+    const db = client.db("todoBase");
+    const collection = db.collection("test");
+    const { name } = req.body;
+
+    // Find the first document in the collection
+    await collection.insertOne({ name });
+  } finally {
+    // Close the database connection when finished or an error occurs
+    await client.close();
+  }
+  res.status(201).json({
+    message: "Thing created successfilly!",
+  });
+});
+
+app.listen(port, async function () {
+  console.log(`Example app listening on port ${port}`);
 });
