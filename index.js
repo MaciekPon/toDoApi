bffe32e11ad155087918d26b3e2d985b358cec15
MC <maciej.p@emiter.net.pl>
2024-10-14 12:05:12 +0200

    add delete routes add env file

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3001;
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const uri =
-  process.env.MONGO_URI ||
-  "mongodb+srv://macia:Porniol1@todobase.yvbuw.mongodb.net/?retryWrites=true&w=majority&appName=todoBase";
+  process.env.MONGO_URI 
+  // || "mongodb+srv://macia:Porniol1@todobase.yvbuw.mongodb.net/?retryWrites=true&w=majority&appName=todoBase";
 
 const client = new MongoClient(uri);
 
@@ -48,18 +48,18 @@ app.post("/addTodo", async function (req, res) {
   });
 });
 
-app.post("/checkTodo", async function (req, res) {
+app.put("/checkTodo", async function (req, res) {
   try {
     await client.connect();
     const db = client.db("todoBase");
     const collection = db.collection("todos");
-    let { todoId } = req.body;
+    let { toDoId } = req.body;
 
-    todoId = new ObjectId(todoId);
+    toDoId = new ObjectId(toDoId);
 
     await collection.updateOne(
       {
-        _id: todoId,
+        _id: toDoId,
       },
       { $set: { isEnded: true } }
     );
@@ -71,6 +71,40 @@ app.post("/checkTodo", async function (req, res) {
     message: "Thing created successfilly!",
   });
 });
+
+app.put("/delete-all", async (req, res) => {
+  await client.connect();
+  const db = client.db("todoBase");
+  const collection = db.collection("todos");
+  let { toDosIds } = req.body
+
+ 
+  try {
+    toDosIds.forEach(async (toDoId) => {
+      toDoId = new ObjectId(toDoId)
+      await collection.deleteOne({ _id: toDoId })
+    })
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+app.delete("/:toDoId", async (req, res) => {
+  await client.connect();
+  const db = client.db("todoBase");
+  const collection = db.collection("todos");
+
+  let { toDoId } = req.params
+  toDoId = new ObjectId(toDoId);
+
+  try {
+    await collection.deleteOne({ _id: toDoId })
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+
 
 app.listen(port, async function () {
   console.log(`Example app listening on port ${port}`);
